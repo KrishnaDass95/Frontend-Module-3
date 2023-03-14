5b24db0c8c06b3b7f3a30ee105d0bb822460495e
KrishnaDass95 <krishdass95@gmail.com>
2023-03-14 22:31:23 +0530

    Part 2 of Promise methods

--- Lectures/promiseMethods.js
+++ Lectures/promiseMethods.js
@@ -8,6 +8,7 @@ let promise1 = new Promise((resolve, reject) => {
 
 let promise2 = new Promise((resolve, reject) => {
     setTimeout(()=>{
+        // reject('could not resolve promise 2')
         resolve('promise2');
     }, 2000)
 })
@@ -56,11 +57,62 @@ let promise3 = new Promise((resolve, reject) => {
 
 // second promise method - "ANY" -> if any of these promises are resolved, do something
 // OH OH, any is whatever is the fastest one resolved, just show that.
-console.log('starting time ', new Date());
-Promise.any([promise1, promise2, promise3]).then((res)=>{
-    console.log('res', res, new Date());
-}).catch((e)=>{
-    console.log('error', e, new Date());
-})
+
+// console.log('starting time ', new Date());
+// Promise.any([promise1, promise2, promise3]).then((res)=>{
+//     console.log('res', res, new Date());
+// }).catch((e)=>{
+//     console.log('error', e, new Date());
+// })
+
 // if all are rejects, we get aggregate ERROR, everything gives an error.
 // it will show whatever passes
+
+// All settled -> keep the code going, show me the states, total time -> sum of all promises
+
+Promise.allSettled([promise1, promise2, promise3]).then((res)=>{
+    console.log('res', res, new Date());
+    res.map((item)=>{
+        const myHTML = `
+        <div class='quote-box'>
+            <h2>${item.status}</h2>
+            <p>${
+                item.status == 'fulfilled' ? item.value : item.reason
+            }</p>
+        </div>
+            `;
+            document.getElementById('container').innerHTML = myHTML;
+    })
+
+})
+
+// All settled is to settle everything, if everything is rejected, there's no error printed
+// it will settle things and just gives the status that it's failed.
+
+
+// Race method -> Race is about the fastest time of which outcome runs the fastest
+// First one to finish 100M, or first one to finish first in swimming.
+// choose the fastest outcome, doesnt matter about resolve or rejection.
+Promise.race([promise1, promise2, promise3])
+.then((res)=>{
+    console.log('race', res);
+})
+
+
+
+
+
+// Summary of Promise methods
+// "all" means - all needs to be resolved.
+// any is fastest resolved, any one of the promise needs to be resolved
+// all-settled shows all the status all the promises in the list
+// race is fastest outcome regardless of success or failure 
+
+
+// advantage of promise methods
+// 1. Promises run parallel
+// 2. We can handle promises in a clean manner using all, any, race or allSettled
+// 3. 
+
+
+
