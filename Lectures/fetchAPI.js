502a1ed648f013b7a56d6a90f50de5bebbf4b6c0
KrishnaDass95 <krishdass95@gmail.com>
2023-03-14 20:08:44 +0530

    fetch chaining

---
+++ Lectures/fetchAPI.js
@@ -0,0 +1,21 @@
+console.log('fetching data');
+
+// API's -> for now an API is a collection of URL's
+// A URL is an address for a computer which runs a specific function, its a specific location. When I go to an address, i recieve data, like google.com, it opens google. Its 
+// an address of a particular computer
+// Internet works the same as the real world postal service
+// What is a server? It is a computer or a set of computers
+// www.google.com -> google.com is the domain -> you can buy an address,
+// every server has a script that runs
+// all URLS return data, either data or a HTML page, etc
+// A lot of URL's make an API(or atleast now)
+
+
+// fetch is a method inbuilt provided by browser which returns a promise
+// using the fetch which returns a promise, we can handle the promise using then and using the res.json() function
+const data = fetch('https://dummyjson.com/quotes/random').then((res)=>res.json()).then((data)=> {
+    console.log('data ->>', data);
+    document.getElementById('container').innerHTML = `<h1>${data.quote}</h1>`;
+});
+
+
