2b17303bea0d18aa8926f46bfdd13532a9fbc759
KrishnaDass95 <krishdass95@gmail.com>
2023-03-16 13:23:32 +0530

    async await part 2

--- Lectures/async.js
+++ Lectures/async.js
@@ -5,7 +5,8 @@ console.log('async');
 function myPromise(){
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
-            resolve('Promise has been resolved');
+            // resolve('Promise has been resolved');
+            reject('promise not resolved')
         }, 2000);
     })
 }
@@ -22,10 +23,16 @@ async function handlePromise(){
     // it will wait as long as the promise is in pending
     // we are making promises go synchronously for us
     // we can control  the execution to go line by line
-    const res = await myPromise(); // the await keyword, is able to change the promise state from pending without resolving it with then
+    const res = await myPromise().catch((e)=> console.log('error', e)); // the await keyword, is able to change the promise state from pending without resolving it with then
     // awaiting for the promise to come
     if(res){ // its nice to add the res in an if condition for extra protection
         console.log('RES>>', res, new Date());
     }
+    // the if also helps when a promise is rejected, because if its errored out, res will be undefined
 }
 handlePromise();
+
+
+// lets look at a rejection case with async await.
+// We need to handle the error with try catch
+// or just catch
