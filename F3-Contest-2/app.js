5ad081f41989680d2bf6d1e6fe37e9bc699fd114
KrishnaDass95 <krishdass95@gmail.com>
2023-03-18 13:01:04 +0530

    Hallelujah, i got the data to show on UI

--- F3-Contest-2/app.js
+++ F3-Contest-2/app.js
@@ -1,6 +1,6 @@
 // Elements
 const fetchDataBtn = document.querySelector('#btn');
-const table = document.querySelector('#data-table');
+const postsTable = document.querySelector('#posts');
 
 function PromiseAPI1(){
     return new Promise((resolve, reject)=>{
@@ -8,9 +8,21 @@ function PromiseAPI1(){
             fetch('https://dummyjson.com/posts')
                 .then((response)=> response.json())
                     .then((data) => {
-                        console.log(data);
+                        console.log('posts object from fetch', data);
+                        posts = data.posts;
+                        console.log('individual posts', posts);
+                        // go through data and show on UI
+                        const tableRows = posts.map((post)=>{
+                            return `
+                            <tr>
+                            <td>${post.id}</td>
+                            <td>${post.title}</td>
+                            <td>${post.body}</td>
+                            </tr>
+                            `
+                        });
+                        postsTable.innerHTML += tableRows.join('');
                     })
-
         })
     })
 
