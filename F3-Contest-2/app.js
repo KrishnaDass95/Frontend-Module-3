08bdc6b02677e315b22a2b0e7e84d1a863499a6e
KrishnaDass95 <krishdass95@gmail.com>
2023-03-18 13:44:27 +0530

    Finish the Promise API functions

--- F3-Contest-2/app.js
+++ F3-Contest-2/app.js
@@ -1,6 +1,8 @@
 // Elements
 const fetchDataBtn = document.querySelector('#btn');
 const postsTable = document.querySelector('#posts');
+const productsTable = document.querySelector('#products');
+const todoTable = document.querySelector('#todos');
 
 function PromiseAPI1(){
     return new Promise((resolve, reject)=>{
@@ -29,17 +31,60 @@ function PromiseAPI1(){
 
 }
 
+function PromiseAPI2(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            fetch('https://dummyjson.com/products')
+                .then(response => response.json())
+                    .then(jsonObject => {
+                        const products = jsonObject.products;
+                        console.log('products object after JSON method', products);
+                        productsArray = products.map((item)=>{
+                            return `
+                            <tr>
+                            <td>${item.brand}</td>
+                            <td>${item.title}</td>
+                            <td>${item.description}</td>
+                            </tr>
+                            `
+                        })
+                        productsTable.innerHTML += productsArray.join('');
+                    })
+        }, 2000)
+    })
 
+}
 
+function PromiseAPI3(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            fetch('https://dummyjson.com/todos')
+                .then(response => response.json())
+                    .then((jsonObject) => {
+                        console.log('todo JSON object ->',jsonObject);
 
-
-
-
-
-
+                        const todoArray = jsonObject.todos;
+                        console.log('todos object array',todoArray);
+                        let todoAddArray = todoArray.map((item)=>{
+                            return `
+                            <tr>
+                            <td>${item.id}</td>
+                            <td>${item.todo}</td>
+                            <td>${item.completed}</td>
+                            </tr>
+                            `
+                        })
+                        todoTable.innerHTML += todoAddArray.join('');
+                    })
+        }, 3000)
+    })
+}
 
 function handlePromiseChain(){
-    PromiseAPI1()
+    // PromiseAPI1();
+    // PromiseAPI2();
+    PromiseAPI3();
+    
 }
 
 fetchDataBtn.addEventListener('click', handlePromiseChain);
