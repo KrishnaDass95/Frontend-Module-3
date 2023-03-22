4294d8f5cea564d0422931dda0264747d6d3e8f1
KrishnaDass95 <krishdass95@gmail.com>
2023-03-22 20:51:57 +0530

    Finish the problem statement, if quotes in local storage, do not fetch

--- Lectures/StorageMain/storageMain.js
+++ Lectures/StorageMain/storageMain.js
@@ -1,13 +1,27 @@
 // Save the 30 quotes in local storage
-// if quotes exist, do not fetch
+// if quotes exist, do not fetch, show data from local storage
 
-fetch("https://dummyjson.com/quotes")
+if(localStorage.getItem('quotes')){
+    console.log('from local storage', localStorage.getItem('quotes'));
+    let quotes = JSON.parse(localStorage.getItem('quotes'));
+    showData(quotes);
+}
+else{
+    fetch("https://dummyjson.com/quotes")
     .then((res)=> res.json())
     .then((data) => {
-        var myOl = document.getElementById('quotes-id');
+        console.log('data from fetching', data);
+        localStorage.setItem('quotes', JSON.stringify(data));
+        showData(data);
+    })
+}
+function showData(data){
+    var myOl = document.getElementById('quotes-id');
         data.quotes.map((quote) => {
             var listItem = document.createElement('li');
             listItem.innerHTML = `${quote.quote}`;
             myOl.appendChild(listItem);
         })
-    })
+
+}
+
